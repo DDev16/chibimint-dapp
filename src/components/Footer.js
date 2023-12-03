1c5b875c8c4f6538045af6fe86dcd78ac0565aea
DDev16 <dillondevera12@gmail.com>
2023-12-02 18:36:11 -0700

    footer push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../styles/Fotter.scss"
 function Footer() {
   return (
-    <footer>
+    <footer style={{ fontFamily: 'BlimeyVariable'}}>
       <p>&copy; 2023 Chaos Creations. All rights reserved.</p>
       <p>Music by <a href="https://www.youtube.com/@mersiv7877" target="_blank" rel="noopener noreferrer">Mersiv</a></p>
     </footer>
